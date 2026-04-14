b6c618b8b27f208cba7e856af2eca3139d8765b9
Rohit <rohitjadhav5802@gmail.com>
2026-04-14 13:55:10 +0530

    5th version of azure pipeline react-app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a Azure Pipeline 
+          This is a Azure Pipeline
+          2nd Version 
         </p>
         <a
           className="App-link"
